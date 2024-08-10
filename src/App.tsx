7185a0d19fcbd97abe2d542eeda5daec753e1fbf
yasg-uru <yashpawar12122004@gmail.com>
2024-08-10 19:51:42 +0530

    integrated a shadcn ui and tested it successfully

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+const  App:React.FC=()=> {
+  return <div className="bg-black min-h-screen">
+    <Button variant="destructive"> this is a </Button>
+  </div>;
+}
+
+export default App;
