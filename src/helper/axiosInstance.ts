4df17e020bcb3c58659319f8182af8ab03c3cbb5
yasg-uru <yashpawar12122004@gmail.com>
2024-09-04 00:37:24 +0530

    changed axios paths api instance

--- src/helper/axiosInstance.ts
+++ src/helper/axiosInstance.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 export const authInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://wordrobe-authentication-service.vercel.app",
   timeout: 1000,
 });
 export const clothInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  // baseURL: "http://localhost:5001",
+  baseURL: "https://wardrobe-wardrobe-service.vercel.app",
 });
