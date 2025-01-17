aad677502f094b17ae8a1abb70c287bebf9d4149
yasg-uru <yashpawar12122004@gmail.com>
2025-01-17 18:59:20 +0530

    corrected get archieved component

--- src/helper/axiosInstance.ts
+++ src/helper/axiosInstance.ts
@@ -11,5 +11,6 @@ import axios from "axios";
 // });
 
 export const axiosInstance =axios.create({
-  baseURL:'http://localhost:5000'
+  // baseURL:'http://localhost:5000'
+  baseURL:'https://wardrobe-pied.vercel.app',
 })
